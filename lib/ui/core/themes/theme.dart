f9cdcbd101de488148f6aebd8923bf65e4c917e8
Reza Farjam <reza00farjam@gmail.com>
2025-02-07 04:16:39 +0330

    style: disable Material 3 in dark theme and change seed color to purple

--- lib/ui/core/themes/theme.dart
+++ lib/ui/core/themes/theme.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
 abstract final class AppTheme {
+  static const _bgColor = Color(0xFF151217);
+
   static ThemeData darkTheme = ThemeData(
-    useMaterial3: true,
+    useMaterial3: false,
+    dialogBackgroundColor: _bgColor,
+    scaffoldBackgroundColor: _bgColor,
     colorScheme: ColorScheme.fromSeed(
-      seedColor: Colors.red,
+      seedColor: Colors.purple,
       brightness: Brightness.dark,
-      dynamicSchemeVariant: DynamicSchemeVariant.monochrome,
     ),
     inputDecorationTheme: const InputDecorationTheme(
       border: OutlineInputBorder(),
-    ),
-    sliderTheme: const SliderThemeData(
-      overlayShape: RoundSliderOverlayShape(overlayRadius: 16),
     ),
   );
 }
